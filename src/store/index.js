1dbc581701cbb4b973837a7b1dc37936f24aeebf
AmmarMasood <ammar.masood98@gmail.com>
2025-04-22 21:57:23 +0500

    fixed

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,8 @@ const state = proxy({
   color: `#EFBD4E`,
   isLogoTexture: true, //are we displaying logo on shirt
   isFullTexture: false,
-  logoDecal: "../../public/zev-log.png",
-  fulldecal: "../../public/zev-log.png",
+  logoDecal: "/zev-log.png",
+  fulldecal: "/zev-log.png",
   clothingSelected: ClothTypes[0].name,
 });
 
